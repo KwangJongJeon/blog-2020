6980727f9408f7ade26fdf192e7cb014122efba4
KJ <abc8339265@gmail.com>
2020-07-29 19:53:32 +0900

    add backend server and got some error about 'CORs policy'

--- blog-2020/src/server/index.js
+++ blog-2020/src/server/index.js
@@ -4,15 +4,25 @@ const cors = require('cors');
 
 const db = require('./db');
 const blogRouter = require('./routes/blog-router');
+const { request } = require('express');
 
 const app = express();
-const apiPort = 3000;
+const PORT = process.env.PORT || 3000;
 
 // TODO:
-// test with postman and ROBO 3T
+// 2020-07-29 
+// need to fix CORS policy error. maybe, I can solve to use a proxy server? 
+// --> /jokes/random proxy server but it's not work 
+// refer to https://medium.com/@dtkatz/3-ways-to-fix-the-cors-error-and-how-access-control-allow-origin-works-d97d55946d9
+
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-origin', '*');
+    next();
+})
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
@@ -20,6 +30,19 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
+app.get('/jokes/random', (req, res) => {
+    request(
+      { url: 'https://joke-api-strict-cors.appspot.com/jokes/random' },
+      (error, response, body) => {
+        if (error || response.statusCode !== 200) {
+          return res.status(500).json({ type: 'error', message: 'error!!!' });
+        }
+  
+        res.json(JSON.parse(body));
+      }
+    )
+  });
+
 app.use('/api', blogRouter);
 
-app.listen(apiPort, () => console.log(`Server running on Port ${apiPort}`));
+app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
