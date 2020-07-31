4d7800228da720c0179614386471159a42b64c62
KJ <abc8339265@gmail.com>
2020-07-31 22:27:51 +0900

    add components index file

--- blog-2020/src/App.js
+++ blog-2020/src/App.js
@@ -3,14 +3,17 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Home from "./Home";
 // import CreateContent from "./components/CreateContent";
 // import UpdateContent from "./components/UpdateContent";
+import { CRUDNavigator } from './components/CRUDNavigator'
 import { ContentList, ContentInsert, ContentUpdate } from './pages';
 
 
 class App extends Component {
   render() {
     return (
+      
       <Router>
         <Switch>
+        
           <Route path="/" exact component={Home}/>
           <Route path="/content/list" exact component={ContentList}/>
           <Route path="/content/create" exact component={ContentInsert}/>
---
+++ blog-2020/src/components/index.js
@@ -0,0 +1,17 @@
+import BulletinBoard from './BulletinBoard';
+import CreateContent from './CreateContent';
+import CRUDNavigator from './CRUDNavigator';
+import DeleteContent from './DeleteContent';
+import ReadContent from './ReadContent';
+import Subject from './Subject';
+import UpdateContent from './UpdateContent';
+
+export {
+    BulletinBoard,
+    CreateContent,
+    CRUDNavigator,
+    DeleteContent,
+    ReadContent,
+    Subject,
+    UpdateContent,
+}
